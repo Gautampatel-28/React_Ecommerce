33c97d3232bc78096fd0e2f4748c606a593e2b7c
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-16 17:46:52 +0530

    deploy

--- src/comp/Nav/Nav.jsx
+++ src/comp/Nav/Nav.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./Nav.css";
 import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-mifbifh7qxsvlbzj.us.auth0.com"
-      clientId="epMwyNUf3HbFGQ3v4wRNuh31fqGIC3Ze"
+      domain="your-auth0-domain"
+      clientId="your-auth0-client-id"
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: `${window.location.origin}/React_Ecommerce/`
       }}
     >
       <App />
