865024fd44b60d1dbd4b52a3d284e73ba2f2dde6
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-16 12:50:32 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_Ecommerce/',
+  base: '/React_Ecommerce/', // Ensure this base path ends with a slash
   server: {
     port: 3000,
   },
