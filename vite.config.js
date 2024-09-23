241222750600bca47dcd8aa8e5596f64d6f529c5
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-23 15:06:25 +0530

    bugFx

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_Ecommerce/', // Ensure this base path ends with a slash
+  base: '/React_Ecommerce/', 
   server: {
     port: 3000,
   },
