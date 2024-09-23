e697b41b69dd06c429d5ab2432c9bba25b0668ee
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-23 15:27:55 +0530

    bugFx

--- src/comp/Router.jsx
+++ src/comp/Router.jsx
@@ -1,20 +1,21 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import Shop from './Shop/Shop';
 import Cart from './Cart/Cart';
 import Contact from './Contact/Contact';
 
-const AppRoutes = ({shop, Filter, allcatefilter, addtocart, cart, setCart}) => {
+const Router = ({shop,Filter, allcatefilter, addtocart, cart, setCart}) => {
   return (
-    <Router>
+    <>
       <Routes>
-        <Route path="/" element={<Home addtocart={addtocart} />} />
+        <Route path="/" element={<Home addtocart={addtocart}/>} />
         <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
-        <Route path="/shop" element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart} />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/shop" element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart} /> } />
+        <Route path="/Contact" element={<Contact /> } />
       </Routes>
-    </Router>
+    </>
   );
 };
 
-export default AppRoutes;
+export default Router;
